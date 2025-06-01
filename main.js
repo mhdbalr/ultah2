ee1c8dfdf07769f875b6e0b9c6338e88b958e258
mhdbalr <muhammad.adeiqbal911@gmail.com>
2025-06-01 12:51:30 +0700

    update splash

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// ... fungsi startScrapbook, updateMessage, prevPage, nextPage
-
 function startScrapbookFromSplash() {
   const splash = document.getElementById('splashScreen');
+  splash.style.transition = 'opacity 1s ease';
   splash.style.opacity = 0;
   setTimeout(() => {
     splash.style.display = 'none';
